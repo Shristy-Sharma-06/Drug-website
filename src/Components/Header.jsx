ec3e20b6b605439d104f05bc37ca28becdcaa26c
Shristy-Sharma-06 <141722046+Shristy-Sharma-06@users.noreply.github.com>
2025-04-17 10:59:20 +0530

    update

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -117,6 +117,7 @@ const Header = () => {
                 </button>
               </li>
             ))}
+            <Product/>
             <More />
 
             <li>
--- src/Components/More.jsx
+++ src/Components/More.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function More() {
   return (
    <>
-   <select className='inline-block px-4 py-2 duration-200 hover:bg-blue-100 rounded-full hover:text-black'>
+   <select className='inline-block px-4 py-2 duration-200 hover:bg-blue-100 rounded hover:text-black'>
     <option value="">More</option>
     <option value="">Join MWN</option>
     <option value="">shop</option>
